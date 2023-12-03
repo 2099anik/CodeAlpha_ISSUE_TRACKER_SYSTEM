c5b4b03e7b58d22007c059888c53389007445335
2099anik <118827373+2099anik@users.noreply.github.com>
2023-12-03 16:58:18 +0530

    Update firebase.js

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -7,12 +7,7 @@ import {getFirestore} from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNsqESe9lCQKCE1FauBBjgz-u4bTD2rkk",
-  authDomain: "pro3-ac406.firebaseapp.com",
-  projectId: "pro3-ac406",
-  storageBucket: "pro3-ac406.appspot.com",
-  messagingSenderId: "1090194810435",
-  appId: "1:1090194810435:web:a032f19bcaea113ae296f6"
+  //your own
 };
 
 // Initialize Firebase
